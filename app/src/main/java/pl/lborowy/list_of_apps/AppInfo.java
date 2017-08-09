b1ab98f0c7fb57af60ddb799e26bf61a094d3d43
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-09 18:43:05 +0200

    AppInfo after Lombok final

--- app/src/main/java/pl/lborowy/list_of_apps/AppInfo.java
+++ app/src/main/java/pl/lborowy/list_of_apps/AppInfo.java
@@ -2,21 +2,14 @@ package pl.lborowy.list_of_apps;
 
 import android.graphics.drawable.Drawable;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
+
 
 /**
  * Created by RENT on 2017-08-09.
  */
 
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
+@Data
 public class AppInfo {
     // klasa AppInfo - reprezentacja aplikacji
     private int uid;
