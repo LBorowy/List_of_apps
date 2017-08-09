2ee0c53e0e5421125e5e1270ef05b2d3184a893c
Łukasz Borowy <lucas.borowy@gmail.com>
2017-08-09 18:41:52 +0200

    AppInfo after Lombok

--- app/src/main/java/pl/lborowy/list_of_apps/AppInfo.java
+++ app/src/main/java/pl/lborowy/list_of_apps/AppInfo.java
@@ -2,62 +2,24 @@ package pl.lborowy.list_of_apps;
 
 import android.graphics.drawable.Drawable;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Created by RENT on 2017-08-09.
  */
 
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class AppInfo {
     // klasa AppInfo - reprezentacja aplikacji
-
     private int uid;
     private String name;
     private Drawable icon;
-
-    public AppInfo(int uid, String name, Drawable icon) {
-        this.uid = uid;
-        this.name = name;
-        this.icon = icon;
-    }
-
-    public int getUid() {
-        return uid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Drawable getIcon() {
-        return icon;
-    }
-
-    @Override
-    public String toString() {
-        return "AppInfo{" +
-                "uid=" + uid +
-                ", name='" + name + '\'' +
-                ", icon=" + icon +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        AppInfo appInfo = (AppInfo) o;
-
-        if (uid != appInfo.uid) return false;
-        if (!name.equals(appInfo.name)) return false;
-        return icon.equals(appInfo.icon);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = uid;
-        result = 31 * result + name.hashCode();
-        result = 31 * result + icon.hashCode();
-        return result;
-    }
 }
